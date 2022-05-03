cee61840ed13899416773a49d400c4028ae732b2
gosha-shapira <52867890+gosha-shapira@users.noreply.github.com>
2022-05-03 20:40:32 +0300

    as the last commit

--- server_with_socket.py
+++ server_with_socket.py
@@ -55,39 +55,6 @@ while True:
         a = pickle.dumps(taged_frame)
         message = struct.pack("Q",len(a))+a
         client_socket.sendall(message)
-        key = cv2.waitKey(5) 
-        if key ==13:
-            client_socket.close()
-
-
-# Clients Code
-# while True:
-#     while len(data) < payload_size:
-#         packet = client_socket.recv(4*1024)
-#         if not packet: break
-#         data+=packet
-#     packed_msg_size = data[:payload_size]
-#     data = data[payload_size:]
-#     msg_size = struct.unpack("Q",packed_msg_size)[0]
-#     while len(data) < msg_size:
-#         data += client_socket.recv(4*1024)
-#     frame_data = data[:msg_size]
-#     data  = data[msg_size:]
-#     frame = pickle.loads(frame_data)
-#     cv2.imshow("Receiving...",frame)
-#     key = cv2.waitKey(10) 
-#     if key  == 13:
-#         break
-# client_socket.close()
-
-
-# vid = cv2.VideoCapture(0)
-# while(vid.isOpened()):
-#     img,frame = vid.read()
-#     a = pickle.dumps(frame)
-#     message = struct.pack("Q",len(a))+a
-#     client_socket.sendall(message)
-#     cv2.imshow('Sending...',frame)
-#     key = cv2.waitKey(10) 
-#     if key ==13:
-#         client_socket.close()
+        # key = cv2.waitKey(5) 
+        # if key ==13:
+        #     client_socket.close()
